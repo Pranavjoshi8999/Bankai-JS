8f634739a8ee7ce58e3851d60d728bbe090d3476
Pranavjoshi8999 <134476623+Pranavjoshi8999@users.noreply.github.com>
2024-01-16 11:12:19 +0000

    stack-Heap

--- 01_basics/datatypes-summery.js
+++ 01_basics/datatypes-summery.js
@@ -5,3 +5,27 @@
 
 //Reference Type/Non Primi
 // Arrys,objects,Functions
+
+
+//memo-2
+// stack (primitive)
+//heap(Non-primitive)
+
+let myName = "Pranav"
+let anotherName=myName
+anotherName = "Madhav"
+console.log(myName);
+console.log(anotherName);
+
+
+let user = {
+    email:"guhbebeu",
+    upi: "ybldbd"
+}
+
+let userTwo = user
+
+userTwo.email = "babsbddggd"
+
+
+
