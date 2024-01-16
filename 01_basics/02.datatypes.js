b814b97a31ad33c5b7285b962b63f64a7c93a0ba
Pranavjoshi8999 <134476623+Pranavjoshi8999@users.noreply.github.com>
2024-01-16 09:39:11 +0000

    data

---
+++ 01_basics/02.datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+
+let name = "Pranav"
+let age = 24
+let isLoggedIn = false
+
+//Premative datatypes
+//number => 2 to power 53 
+// bigint
+// string => ""
+// boolean = true/false
+// null = stand alone value
+// undefined
+// symbol = unique
+
+// object 
+
+console.log(typeof age);
+console.log(typeof null);// tyep object
+console.log(typeof undefined);// type undefined
