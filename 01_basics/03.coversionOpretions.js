dc2f615316b8ea7457190e55620592de039b4385
Pranavjoshi8999 <134476623+Pranavjoshi8999@users.noreply.github.com>
2024-01-16 10:32:13 +0000

    w

--- 01_basics/03.coversionOpretions.js
+++ 01_basics/03.coversionOpretions.js
@@ -29,3 +29,17 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//*****************Operations********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
